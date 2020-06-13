504c94011da041bcd3d259a85717bbb59e35b789
Bruce Parker <detailsharks@gmail.com>
2020-06-13 00:57:55 +0000

    Automatically backed up by Learn

---
+++ lib/shoe.rb
@@ -0,0 +1,13 @@
+class Shoe
+  attr_accessor :brand, :color, :size, :material :condition
+  attr_reader :cobble
+  
+  def intialize(brand)
+    @brand = brand
+  end
+  
+  def cobbler
+    puts "says the shoe has been repaired", "makes the shoe's condition new"
+  end
+  
+end
